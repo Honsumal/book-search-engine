bf61a9f8fae8e13cb7b5f382d693495ca3109f7d
honsumal <14leealastair@gmail.com>
2022-11-21 13:59:53 -0500

    finally fixed context bug

--- server/server.js
+++ server/server.js
@@ -5,12 +5,14 @@ const mongoose = require('mongoose')
 
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware
 });
 
 
